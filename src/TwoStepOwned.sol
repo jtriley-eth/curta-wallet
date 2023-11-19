f0b65bf1b06565eac8e9d00e295a48ef39ba6312
jtriley.eth <Jtriley15@gmail.com>
2023-11-19 18:37:39 +0100

    add auth

--- src/TwoStepOwned.sol
+++ src/TwoStepOwned.sol
@@ -1,14 +1,28 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.22;
 
-contract TwoStepOwned {
+/// @title Two Step Owned
+/// @author jtriley.eth
+/// @dev two-step ownership transfer.
+abstract contract TwoStepOwned {
+    /// @dev logged when ownership transfer starts.
+    /// @param previous previous owner.
+    /// @param next next owner.
     event StartOwnerTransfer(address indexed previous, address indexed next);
+
+    /// @dev logged when ownership transfer is complete.
     event CompleteOwnerTransfer();
 
+    /// @dev thrown when the caller is not the owner.
     error NotOwner();
+
+    /// @dev thrown when the caller is not the next owner.
     error NotNextOwner();
 
+    /// @dev contract owner.
     address public owner;
+
+    /// @dev next contract owner (when transferring).
     address public next;
 
     modifier onlyOwner() {
@@ -16,6 +30,8 @@ contract TwoStepOwned {
         _;
     }
 
+    /// @dev starts ownership transfer. throws if caller is not owner.
+    /// @param nextOwner next owner.
     function startOwnerTransfer(address nextOwner) public {
         address currentOwner = owner;
         if (currentOwner != msg.sender) revert NotOwner();
@@ -23,6 +39,7 @@ contract TwoStepOwned {
         emit StartOwnerTransfer(currentOwner, nextOwner);
     }
 
+    /// @dev completes ownership transfer. throws if caller is not next owner.
     function completeOwnerTransfer() public {
         address nextOwner = next;
         if (nextOwner != msg.sender) revert NotNextOwner();
